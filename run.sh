3b58d97f3c735841e96d74e6f2e82b95b4ef6911
mohemohe <mohemohe@ghippos.net>
2019-07-01 14:27:40 +0900

    add ssh, awscli

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-docker run --rm -it -e UID="$UID" -e GID="$GID" -e USER="$(whoami)" -v /var/run/docker.sock:/var/run/docker.sock dev
+docker run \
+       --rm \
+       -it \
+       -e UID="$UID" \
+       -e GID="$GID" \
+       -e USER="$(whoami)" \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       -v "${HOME}/.ssh:/home/$(whoami)/.ssh" \
+       -v "${HOME}/.aws:/home/$(whoami)/.aws" \
+       dev
